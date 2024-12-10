e63fecddfbdb4da559f7f64f357f38ed3a0e4634
meethigher <meethigher@qq.com>
2024-12-10 22:17:50 +0800

    feat: km算法实现最大权完美匹配

--- src/main/java/MaximumMatching.java
+++ src/main/java/MaximumMatching.java
@@ -1,5 +1,11 @@
 
 
+/**
+ * 使用匈牙利算法请求二分图的最大匹配
+ *
+ * @author <a href="https://meethigher.top/blog/2024/hungarian-algorithm/">匈牙利算法解决整体最优分配问题 - 言成言成啊</a>
+ * @since 2024/12/10 22:14
+ */
 public class MaximumMatching {
 
     /**
---
+++ src/main/java/MaximumWeightPerfectMatching.java
@@ -0,0 +1,11 @@
+
+/**
+ * 使用KM算法请求二分图的最大权完美匹配
+ *
+ * @author <a href="https://meethigher.top/blog/2024/hungarian-algorithm/">匈牙利算法解决整体最优分配问题 - 言成言成啊</a>
+ * @since 2024/12/10 22:15
+ */
+public class MaximumWeightPerfectMatching {
+
+
+}
